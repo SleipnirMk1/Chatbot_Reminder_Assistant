93842f7be58c25a4409f86bf383d49189bc573c4
yudialfayat <ayudi3521@gmail.com>
2021-04-28 15:10:47 +0700

    harus menganduk kata "topik"

--- script/topic.js
+++ script/topic.js
@@ -1,26 +1,37 @@
 function getTopic(message) {
-    var kataPenting = "";
-    var idxKataPenting = -1;
-    for (const key in listKataPenting) {
-        const element = listKataPenting[key];
-        // Harusnya boyormoore
-        idxKataPenting = message.indexOf(element);
-        if (idxKataPenting != -1) {
-            kataPenting = element;
-            break;
-        }
-    }
+    // var kataPenting = "";
+    // var idxKataPenting = -1;
+    // for (const key in listKataPenting) {
+    //     const element = listKataPenting[key];
+    //     // Harusnya boyormoore
+    //     idxKataPenting = boyerMoore(message, element);
+    //     if (idxKataPenting != -1) {
+    //         kataPenting = element;
+    //         break;
+    //     }
+    // }
 
-    if (idxKataPenting == -1) {
-        console.log("Kata penting tidak ditemukan");
-        return "null";
+    // if (idxKataPenting == -1) {
+    //     console.log("Kata penting tidak ditemukan");
+    //     return "null";
+    // }
+
+    var idxKataTopik = boyerMoore(message, "topik");
+    var idx = idxKataTopik;
+    var space = false;
+    while (message[idx] == " " || !space) {
+        if (message[idx] == " ") {
+            space = true;
+        }
+        idx++;
     }
 
     var idxWaktu = -1;
     for (const key in listMelihatDaftarTask) {
         const element = listMelihatDaftarTask[key];
         // Harusnya boyormoore
-        idxWaktu = message.indexOf(element);
+        // console.log(element);
+        idxWaktu = boyerMoore(message, element);
         if (idxWaktu != -1) {
             break;
         }
@@ -33,9 +44,9 @@ function getTopic(message) {
 
     // var count = kataPenting.length;
 
-    if (idxWaktu > idxKataPenting) {
-        return message.substr(idxKataPenting, idxWaktu - idxKataPenting);
+    if (idxWaktu > idx) {
+        return message.substr(idx, idxWaktu - idx);
     } else {
-        return message.substr(idxKataPenting, message.length - idxKataPenting);
+        return message.substr(idx, message.length - idx);
     }
 }
